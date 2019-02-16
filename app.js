f19ccb2d1e380e90f39ee014b82d04b8b6439ad3
Andrew <healeycodes@gmail.com>
2019-02-16 00:15:41 +0000

    Change serviceAccountKey to be injectable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Firebase */
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // .gitignored
+const serviceAccount = require(process.env.KEY);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.DB
